6ca5673bcb00013f43548719ef4bf40cc7534459
Al Muksid Alam Alvi <68986966+Al-Muksid@users.noreply.github.com>
2020-08-02 19:00:22 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+class UI {
+    constructor(){
+        this.budgetFeedback = document.querySelector(".budget-feedback");
+        this.expenseFeedback = document.querySelector(".expense-feedback");
+        this.budgetForm = document.querySelector("budget-form");
+        this.budgetInput = document.querySelector("budget-form");
+        this.budgetAmount = document.querySelector("budget-form");
+        this.expenseAmount = document.querySelector("budget-form");
+        this.balance = document.querySelector("budget-form");
+        this.balanceAmount = document.querySelector("budget-form");
+        this.expenseForm = document.querySelector("budget-form");
+        this.expenseInput = document.querySelector("budget-form");
+        this.amountInput = document.querySelector("budget-form");
+        this.expenseList = document.querySelector("budget-form");
+        this.itemList = document.querySelector("budget-form");
+        this.itemList = [];
+        this.itemID = 0;
+
+    }
+}
